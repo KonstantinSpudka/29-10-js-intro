d54d1ad1d2af15a4c77c61feabbf675f8edca9ef
KonstantinSpudka <kspudka@gmail.com>
2022-10-29 16:39:31 +0300

    Add index.js

---
+++ js/index.js
@@ -0,0 +1,28 @@
+//Виведення в консоль
+console.log("Hello, world");
+
+//вывести в локальное окно
+//alert("Hello, world");
+
+/*багато строковий коментар */
+
+//змінні
+
+let a = 1;
+console.log("a =", a);
+a = a + 1;
+console.log("a =", a);
+
+const pi = 3.14; //Неможно перепристворювати значення
+console.log(pi);
+var b = 10;
+console.log("b =", b);
+
+// Стиль іменування camelCase
+// lowerCamelCase
+// UpperCamelCase
+
+// PI, MATH_PI
+
+// 1 a-z, A-Z, _, $
+// 2 ... a-z, A-Z, 0-9, _, $
